68c7659b465c0c870ed5d861257e47581ad6938c
Gosha Krikun <g.krikun@innopolis.ru>
2025-06-25 10:06:35 +0300

    feat: update manifest for plugin publishing

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,7 +1,11 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
   name: "G-code generation | GRBL/Marlin lasers",
-  id: "1222852692367737510",
+  id: "1519595187088702528",
+  networkAccess: {
+    allowedDomains: ["none"],
+  },
+  documentAccess: "dynamic-page",
   api: "1.0.0",
   main: "plugin.js",
   ui: "index.html",
